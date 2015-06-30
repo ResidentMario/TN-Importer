0d45b16d175eaa95ade3f6636f6a2bbfede2dfd7
Aleksey Bilogur <residentmario@gmail.com>
2015-06-29 22:23:53 -0400

    Update signpostlib.py

--- signpostlib.py
+++ signpostlib.py
@@ -1,23 +1,14 @@
 # This library provides a loose collection of methods useful for tasks associated with the Wikipedia Signpost.
 
-# Prerequisites to using this script:
+# Prerequisites to using this library:
 # You must have the `pywikibot` package installed and set up.
 # Furthermore it is recommended that you edit `user_config.py` directly to contain as many global definitions as possible, ae.:
 # + usernames['wikipedia']['*'] = u'Resident Mario'
 # + usernames['commons']['*'] = u'Resident Mario'
 # + ...
 
-# This script uses the following notable packages and capacities.
-# pywikibot:			Handles all writing to the projects.
-# requests:				Used to handles API query methods in this file.
-
-# LIMITATION: Currently the API query methods only accept `action=query&prop=(*)` requests. Need to code in `action=query&list=(*)` requests.
-
 import pywikibot
-# import sys
 import requests
-import json
-# import time
 import datetime
 
 # SEEKER METHOD: Finds the next Signpost issue date.
@@ -66,47 +57,27 @@ def getPageWikicode(page, language='en', project='wikipedia'):
 def htmlToWikitext(html):
 	return requests.post('https://rest.wikimedia.org:443/en.wikipedia.org/v1/transform/html/to/wikitext', data={'html': html}).text
 
-# EXECUTION METHOD: A simple API query method which does not implement continuation parameters.
+# EXECUTION METHOD: A light wrapper of `pywikibot.data.api.Requests` that implements free-form JSON API queries.
 # PARAMETERS:
-# (req) target: 		Target page of the project.
-# (req) list_or_prop:	Either `prop` or `list`, depending on what kind of query is being made. eg. the `prop` in `[...]&prop=categories`
-# (req) list_or_prop_param:	The body of the `prop` or `list` command. eg. the `categories` in `[...]&prop=categories`
 # (opt) language:		Language of the project, en is the default.
 # (opt) project:		Project, wikipedia is the default.
 # (kwr) _params:		Additional parameters passed to the query.
-# NOTE: This method does not implement continuation. It implements only `query` commands.
-def makeSimpleAPIQuery(target, list_or_prop, list_or_prop_param, language='en', project='wikipedia', **_params):
-	returnable = requests.get('https://' + language + '.' + project + '.org/w/api.php?&action=query&format=json&formatversion=2&continue=&' + list_or_prop + '=' + list_or_prop_param + '&titles=' + target, params=_params)
-	returnable = json.loads(returnable.text)
-	return returnable['query']['pages'][0][list_or_prop_param]
+def makeRawAPIQuery(language='en', project='wikipedia', **_params):
+	_site = pywikibot.Site(language, project)
+	_params.update({'formatversion': '2', 'continue': ''})
+	return pywikibot.data.api.Request(site=_site, **_params).submit()
 
-# EXECUTION WRAPPER METHOD: An API query method which implements continuation parameters. Just a wrapper for `__makeAPIQuery()`.
-# PARAMETERS:
-# (req) target: 		Target page of the project.
-# (req) prop:			API query `prop` parameter.
+# EXECUTION METHOD: A heavy wrapper of `pywikibot.data.api.Requests` that makes use of the raw method above. Decapsulates requested data.
+# Currently only works for `query` requests.
 # (opt) language:		Language of the project, en is the default.
 # (opt) project:		Project, wikipedia is the default.
 # (kwr) _params:		Additional parameters passed to the query.
-# DO NOT USE: Though it works I want to generalize this function before I use it.
-def makeAPIQuery(target, prop, language='en', project='wikipedia', **_params):
-	return __makeAPIQuery(target, prop, _params, language, project)
-
-# WRAPPED METHOD: Called by makeAPIQuery. Basically only here to convert away from a keyword argument, which can't be dealt with recursively.
-# Implements continuation.
-# See the above for parameterization.
-# DO NOT USE: Though it works I want to generalize this function before I use it.
-def __makeAPIQuery(target, prop, continue_information, language='en', project='wikipedia'):
-	concat_string = ''
-	for item in continue_information:
-		concat_string += '&' + item + '=' + continue_information[item]
-	returnable = requests.get('https://' + language + '.' + project + '.org/w/api.php?&action=query&format=json&formatversion=2&continue=&prop=' + prop + '&titles=' + target + concat_string)
-	returnable = json.loads(returnable.text)
-	if 'continue' not in returnable:
-		return returnable['query']['pages'][0][prop]
-	else:
-		_continue_information = continue_information.copy()
-		_continue_information.update(returnable['continue'])
-		return returnable['query']['pages'][0][prop] + __makeAPIQuery(target, prop, _continue_information, language='en', project='wikipedia')
+def makeAPIQuery(language='en', project='wikipedia', **_params):
+	if 'action' in _params and _params['action'] == 'query':
+		if 'prop' in _params:
+			return makeRawAPIQuery(language, project, **_params)['query']['pages'][0][_params['prop']]
+		elif 'list' in _params:
+			return makeRawAPIQuery(language, project, **_params)['query'][_params['list']]
 
 # EXECUTION METHOD: Writes the contents of a string to a page on a project.
 # PARAMETERS:
@@ -119,7 +90,7 @@ def __makeAPIQuery(target, prop, continue_information, language='en', project='w
 def saveContentToPage(content, target, editsummary, language='en', project='wikipedia'):
 	site = pywikibot.Site(language, project)
 	page = pywikibot.Page(site, target)
-	text = page.text
+	text = page.texts
 	page.text = content
 	page.save(editsummary)
 
@@ -133,4 +104,6 @@ def saveContentToPage(content, target, editsummary, language='en', project='wiki
 # print(getNextSignpostPublicationString())
 # print(getPreviousSignpostPublicationString())
 # print(getPageWikicode('Paris'))
+# print(makeAPIQuery(project='meta', action='query', prop='links', titles='Tech/News/Latest'))
+# print(makeAPIQuery(project='meta', action='query', list='alllinks', alfrom='B', alprop='ids|title'))
 # print("Done.")
